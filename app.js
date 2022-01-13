109d0bd2ef983c3b3d10ab6ea80c528b693a48dc
oregu1 <68478325+oregu1@users.noreply.github.com>
2022-01-13 21:45:24 +0700

    Added some content and styles

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('works')
+//1. Get needed elements
+const categoryBtns = document.querySelectorAll('.caterory-btn');
+const categoryElems = document.querySelectorAll('.category-item');
+
+categoryBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        const item = e.currentTarget.dataset.cat;
+        categoryElems.forEach(elem => {
+            if(elem.classList.contains(item)) {
+                elem.style.display = 'block'
+            } else if(item === 'all') {
+                elem.style.display = 'block'
+            } else {
+                elem.style.display = 'none'
+            }
+        })
+    })
+})
